31807df305c9e3fa0096ca95491fdb5b60b237eb
srikanth <srikanthrajalingam555@gmail.com>
2024-12-09 19:04:33 +0530

    added lambdafunct.py

---
+++ lambdaworks.py
@@ -0,0 +1,19 @@
+"""x=lambda x,y:x+y
+print(x(2,3))"""
+
+# x=lambda x: x^2 + 15
+# print(x(2))
+
+
+# tup=[1,3,2,5,4]
+# x=lambda tup : tup.sort()
+# x(tup)
+# print(tup)
+
+# a=[1,2]
+# b=[1,2,3]
+# l=list(filter(lambda x:x in a,b)
+# print(l)/
+
+
+
