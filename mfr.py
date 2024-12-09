9c73c4b18b63c52e9e1afb5b3179adf1db8726fd
srikanth <srikanthrajalingam555@gmail.com>
2024-12-09 19:18:42 +0530

    map filter and reduce

---
+++ mfr.py
@@ -0,0 +1,19 @@
+# map 
+# def mul2(x):
+#     return x*2
+# l=[1,2,3,4]
+# ml=map(mul2,l)
+# print(list(ml))
+
+
+#filter
+
+# l=[3,4,5,6,9,12,16]
+# m=list(filter(lambda x:x%3==0,l))
+# print([int(i/3) for i in m])
+
+# reduce 
+# from functools import reduce
+# l=[1,2,3,4,5,6,7,8,9]
+# p=reduce(lambda x,y:x+y,l)
+# print(p,'   ',sum(l))
