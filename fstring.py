3acca130ddea7617b42c66703234a563da74ca41
srikanth <srikanthrajalingam555@gmail.com>
2024-12-09 19:02:52 +0530

    added fstrings

---
+++ fstring.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+
+name:str='srikanth'
+age:int=21
+clg:str='SRM'
+decimal:float=1.2034578
+percent:float=.236478
+
+big=123543654
+
+#fname=
+"print(f'name is {name=} and age is {age=}')"
+
+#decimal rounding 
+"""print(f"decimal is {decimal:.2f}")
+print(f"decimal is {percent:.3%}")"""
+
+# bignumbers
+#print(f"{big:,}")
+
+#datetime
+"""now=datetime.now()
+print(f"now normal:{now}")
+print(f"now Complete:{now:%c}")
+print(f"now H:{now:%H}")
+print(f"now date:{now:%D}")
+print(f"now year:{now:%Y}")
+"""
+
